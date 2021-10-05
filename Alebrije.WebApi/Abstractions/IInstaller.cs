f02cc61bce51c89b0a0d116bdb7d1ffd467253b1
JJ Antonio Franco <jj.antonio@alebrije.dev>
2021-10-04 22:56:23 -0500

    Base API implementation

---
+++ Alebrije.WebApi/Abstractions/IInstaller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alebrije.WebApi.Abstractions
+{
+    public interface IInstaller
+    {
+        void InstallServices(IServiceCollection services, IConfiguration configuration);
+    }
+}
---
+++ Alebrije.WebApi/Settings/VersionSettings.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Alebrije.WebApi.Settings
+{
+    public class VersionSettings
+    {
+        public string Name { get; set; }
+        public IEnumerable<string> AllowedVersions { get; set; }
+    }
+}
