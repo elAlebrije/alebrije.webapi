cc5c4ed96ba08997bd87098bda38c1e75207a9e2
JJ Antonio Franco <jj.antonio@alebrije.dev>
2021-10-05 19:33:49 -0500

    Missing HealhMetricsInstraller

---
+++ Alebrije.WebApi/Installers/HealthMetricsInstaller.cs
@@ -0,0 +1,16 @@
+﻿using Alebrije.WebApi.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alebrije.WebApi.Installers
+{
+    public class HealthMetricsInstaller : IInstaller
+    {
+        public void InstallServices(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMetrics();
+            services.AddHealthChecks();
+        }
+
+    }
+}
